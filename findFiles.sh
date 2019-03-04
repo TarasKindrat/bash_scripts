0c48eb630387d206added24b2d2b49c5fde5cd8f
TarasKindrat <kindrat5@gmail.com>
2019-03-04 10:11:30 +0200

    add new scripts

---
+++ findFiles.sh
@@ -0,0 +1,12 @@
+#! bin/bash
+
+cd /home/taras/20files 
+for file in *
+  do
+   echo $file;
+done
+   tar -cvzf files.tar.gz *
+# detele files.ter.gz olde 30 minutes  (-mmin +30 it is -mtime in minutes)
+find files.tar.gz -mmin +30 -exec rm -rf {} \;
+
+
---
+++ logs.sh
@@ -0,0 +1,29 @@
+﻿#! bin/bash 
+
+cd /var/log
+ 
+find . -type f -name "*" -print |
+  while read file
+ do
+
+	if [ $(grep -irnw 'error|warn' $file | wc -l) -gt 0 ] # chek if is some errore or warn in file
+
+            then
+      printf "\n" >> /home/taras/scripts/errors.txt
+      
+              echo $file >> /home/taras/scripts/errors.txt
+      
+              echo "===================">> /home/taras/scripts/errors.txt
+      
+              grep -inwC 3 error $file >> /home/taras/scripts/errors.txt 
+      
+              echo "===================">> /home/taras/scripts/errors.txt
+      
+              echo `date +%H:%M:%S`  >> /home/taras/scripts/errors.txt
+     
+              printf '\n' >> //home/taras/scripts/errors.txt
+    
+           fi
+
+  done
+
