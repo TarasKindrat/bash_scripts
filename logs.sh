4774ee7d62a8631414f54a5495f50a619db15dc7
Taras <kindrat5@gmail.com>
2019-03-05 11:33:23 +0200

    Update logs.sh
    add warn line

--- logs.sh
+++ logs.sh
@@ -7,8 +7,8 @@ find . -type f -name "*" -print |
    do
 
 	if [ $(grep -irnw 'error|warn' $file | wc -l) -gt 0 ] # chek if is some errore or warn in file
-
-            then
+           # print in file rows whis errors and warns and 3 rows before and 3 after
+            then 
               printf "\n" >> /home/taras/scripts/errors.txt
       
               echo $file >> /home/taras/scripts/errors.txt
@@ -16,6 +16,7 @@ find . -type f -name "*" -print |
               echo "===================">> /home/taras/scripts/errors.txt
       
               grep -inwC 3 error $file >> /home/taras/scripts/errors.txt 
+	      grep -inwC 3 warn $file >> /home/taras/scripts/errors.txt
       
               echo "===================">> /home/taras/scripts/errors.txt
       
